e074017a284e9056bcd127c00099eed6c82eddff
Mathisgmn <157029434+Mathisgmn@users.noreply.github.com>
2026-01-15 07:11:43 +0100

    Add GitLab CI lint jobs

---
+++ front/.eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: 2021,
+    sourceType: "module",
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+  rules: {
+    "no-unused-vars": "warn",
+  },
+};
